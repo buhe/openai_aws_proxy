de377d3c62308cc3e55738ed4aed1f603877f721
buhe <bugu1986@126.com>
2023-03-10 19:49:47 +0800

    git-gpt:update

--- api/gpt.ts
+++ api/gpt.ts
@@ -3,7 +3,6 @@ import type { VercelRequest, VercelResponse } from '@vercel/node'
 
 
 module.exports = async (request: VercelRequest, response: VercelResponse) => {
-    console.log(JSON.stringify(request.headers))
     const options = {
         method: 'POST',
         headers: {
